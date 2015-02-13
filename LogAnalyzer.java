319468c45c30cb76ecdda160314387cea344e6f9
Sourdaci <sourdaci@gmail.com>
2015-02-13 12:24:53 +0100

    LogAnalyzer.quietestHour() devuelve la hora con menos accesos al servidor

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -70,6 +70,19 @@ public class LogAnalyzer{
     }
     
     /**
+     * Devuelve la hora con el menor numero de accesos registrados
+     */
+    public int quietestHour(){
+        int indice = 0;
+        for (int index = 0; index < hourCounts.length; index++){
+            if (hourCounts[index] < hourCounts[indice]){
+                indice = index;
+            }
+        }
+        return indice;
+    }
+    
+    /**
      * Print the hourly counts.
      * These should have been set with a prior
      * call to analyzeHourlyData.
